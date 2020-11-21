1342a60c5905cb02ed26381c9f98b29acb7208ec
Tiffany G <guthriet@lanecc.edu>
2020-11-20 21:00:44 -0800

    Added all views and controllers. Edited the Nav Bar to be functional with bare views.

--- CS295_Term/CS295_Term/Controllers/CartController.cs
+++ CS295_Term/CS295_Term/Controllers/CartController.cs
@@ -1,26 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
 
 namespace CS295_Term.Controllers
 {
-    public class HomeController : Controller
+    public class CartController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult CompletedList()
         {
             return View();
         }
-
     }
 }
--- CS295_Term/CS295_Term/Controllers/EditorController.cs
+++ CS295_Term/CS295_Term/Controllers/EditorController.cs
@@ -1,26 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
 
 namespace CS295_Term.Controllers
 {
-    public class HomeController : Controller
+    public class EditorController : Controller
     {
-
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
     }
 }
